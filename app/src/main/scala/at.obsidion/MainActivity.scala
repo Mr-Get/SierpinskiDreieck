85301a4359f9eae9e24cb5c7a758dffd8fb6cca6
Mr-Get <ValarM11rghulis>
2016-12-05 19:45:14 +0100

    2. Commit

---
+++ app/src/main/scala/at.obsidion/MainActivity.scala
@@ -0,0 +1,12 @@
+package at.obsidion
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity extends Activity {
+
+  override protected def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
